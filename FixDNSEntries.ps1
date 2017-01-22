2a22e17a7a1e4e9dc862346926ab540e83f615f1
Cort A. Schaefer <cort@eschaefer.net>
2017-01-22 14:02:33 -0700

    remove the check for dhcp assigned interfaces

--- FixDNSEntries.ps1
+++ FixDNSEntries.ps1
@@ -1,7 +1,7 @@
 # this script is to deal with the fact that windows containers always takes the hosts vEthernet ip as their first DNS entry regardless of how docker is configured to run
 # https://github.com/docker/docker/issues/30260
 
-$if = get-netipinterface -AddressFamily IPv4 -Dhcp Enabled -ConnectionState Connected
+$if = get-netipinterface -AddressFamily IPv4 -ConnectionState Connected
 $ifIndex = $if[0].ifIndex
 #$ifIndex
 
